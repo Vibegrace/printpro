70d44a5feb02787622f1ce7157433eb7359c3bde
Vibegrace <218416273+Vibegrace@users.noreply.github.com>
2026-08-01 15:39:03 +0000

    Fix PHPMailer require paths in API files (use ../PHPMailer/src/)
    Co-authored-by: kiloconnect[bot] <240665456+kiloconnect[bot]@users.noreply.github.com>

--- api/checkout.php
+++ api/checkout.php
@@ -12,9 +12,9 @@ require_once '../config.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
 
 /* =========================
    ROUTE REQUEST
--- api/ordering-system.php
+++ api/ordering-system.php
@@ -10,9 +10,9 @@ require_once '../config.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
 
 $action = $_POST['action'] ?? '';
 
